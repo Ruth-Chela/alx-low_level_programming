065a4d946736a022d8301e4fe234e7eea45d5e48
Ruth-Chela <@karutto18@gmail.com>
2023-02-16 12:13:30 -0800

    4th dimension

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main -print a line of code
+ * main - Entry point
  *
- * return: 0
+ * return:Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	 puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }	
